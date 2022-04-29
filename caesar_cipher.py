b0d0e820c401dda54debf4a9bbc17f97035d8d0b
p22mgiroux <p22mgiroux@fairfieldprep.org>
2022-04-28 23:26:10 -0400

    Submit Cypher

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -14,12 +14,36 @@ def cipher_key(shift):
 
 def shift_line(line, dict_key):
     new_line = ""
-    # Add code here
+    for letter in line:
+        if letter == " ":
+            new_line += " "
+            continue
+        elif letter == "\n":
+            new_line += "\n"
+            continue
+        elif not letter.isalpha():
+            new_line += letter
+            continue
+
+        
+        letter = letter.upper()
+        new_line += dict_key[letter]
+
     return new_line
 
 
 def encrypt_message(filename, dict_key):
-    # Add code here
+    fixed = []
+    final = ""
+    with open(filename) as file:
+        for line in file:
+            fixed += shift_line(line,dict_key) # uses key to encrypt
+        for lines in fixed:
+            final = final + lines
+        file = open("encrypted_{}".format(user_file),"w")
+        file.write(final) # adds to final and writes in new .txt
+        file.close()
+
 
 
 # Main
